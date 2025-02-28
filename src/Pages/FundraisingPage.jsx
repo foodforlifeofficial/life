6cb82e3b3e6b7566c4549638989883c1fb0b3255
foodforlifeofficial <foodforlife.official0@gmail.com>
2025-02-28 16:28:45 +0200

    fix error

--- src/Pages/FundraisingPage.jsx
+++ src/Pages/FundraisingPage.jsx
@@ -1,14 +1,6 @@
 import { Grid2, Container, Typography, Box } from "@mui/material";
 import DonationCard from "../Components/DonationCard";
 import { styled } from "@mui/system";
-import palestine from "/palestine.jpg";
-import etiopia from "/etiopia.jpg";
-import sudan from "/sudan.jpg";
-import ukraine from "/ukraine.jpg";
-import afgha from "/afgha.jpg";
-import yemen from "/yemen.jpg";
-import syria from "/syria.jpeg";
-import somalia from "/somalia.jpg";
 
 const Section = styled(Box)({
   padding: "10px 0",
@@ -19,56 +11,63 @@ export default function FundraisingPage({ darkMode }) {
   const donationData = [
     {
       title: "Sudan: Help families conflict",
-      image: sudan,
+      image:
+        "https://www.sos-childrensvillages.org/getmedia/0f616f64-15f7-434e-96cf-6993f8246483/MAIN-Khartoum-Sudan-People-fleeing-fighting-AFP.jpg?width=1920&height=1080&ext=.jpg",
       totalMeals: 400000,
       currentMeals: 224000, // 56%
       supporters: 57989,
     },
     {
       title: "Palestine: Provide food aid",
-      image: palestine,
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXDgZxJ_i7AbU6ZUEu7y1KwM-MntQErEgUOQ&s",
       totalMeals: 350000,
       currentMeals: 82435, // 55%
       supporters: 98456,
     },
     {
       title: "Ukraine: Support families",
-      image: ukraine,
+      image:
+        "https://d3i6fh83elv35t.cloudfront.net/static/2023/02/GettyImages-1246057605-1024x683.jpg",
       totalMeals: 250000,
       currentMeals: 72320, // 51%
       supporters: 76532,
     },
     {
       title: "Ethiopia: Combat crisis",
-      image: etiopia,
+      image:
+        "https://s.abcnews.com/images/Politics/tigray-gty-rc-210527_1622120835745_hpMain.jpg",
       totalMeals: 380000,
       currentMeals: 201400, // 53%
       supporters: 65987,
     },
     {
       title: "Yemen: Provide emergency aid",
-      image: yemen,
+      image:
+        "https://www.arabnews.com/sites/default/files/styles/n_670_395/public/2020/01/01/1909711-842396305.jpg?itok=F2fCoBHa",
       totalMeals: 450000,
       currentMeals: 252000, // 56%
       supporters: 82934,
     },
     {
       title: "Somalia: Fight famine",
-      image: somalia,
+      image:
+        "https://static01.nyt.com/images/2022/12/05/multimedia/00somalia-famine-1-1-93b0/00somalia-famine-1-1-93b0-videoSixteenByNineJumbo1600.jpg",
       totalMeals: 330000,
       currentMeals: 171600, // 52%
       supporters: 54238,
     },
     {
       title: "Syria: Humanitarian relief",
-      image: syria,
+      image:
+        "https://disasterphilanthropy.org/wp-content/uploads/2022/09/Fb6_3TEWQAEldlj.jpeg",
       totalMeals: 410000,
       currentMeals: 221400, // 54%
       supporters: 67892,
     },
     {
       title: "Afghanistan: Help displaced people",
-      image: afgha,
+      image: "https://www.unhcr.org/sites/default/files/RF1318822.jpg",
       totalMeals: 450000,
       currentMeals: 243000, // 54%
       supporters: 94567,
